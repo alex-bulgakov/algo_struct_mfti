3f4b37ccafe0e8a4fe1b4a35d2887d0cbde774dc
alebul <master8423@gmail.com>
2020-11-25 13:28:15 +0300

    ex03 done

---
+++ lec01/lab/ex02_letter_s.py
@@ -0,0 +1,15 @@
+import turtle
+from time import sleep
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+
+sleep(2)
---
+++ lec01/lab/ex03_square.py
@@ -0,0 +1,8 @@
+import turtle as tl
+from time import sleep
+
+for i in range(4):
+    tl.forward(100)
+    tl.left(90)
+
+sleep(2)
