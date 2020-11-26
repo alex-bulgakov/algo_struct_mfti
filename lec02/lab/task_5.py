f37249e00301c71ef4e2e37655185b391debcf64
alebul <master8423@gmail.com>
2020-11-26 14:48:18 +0300

    done task 06

--- lec02/lab/task_5.py
+++ lec02/lab/task_5.py
@@ -3,19 +3,6 @@
 from pyrob.api import *
 
 
-def detect_direction():
-    """
-
-    :return: True if walls on Right, False if walls on Left
-    """
-    move_right()
-    if wall_is_beneath():
-        move_left()
-        return True
-    else:
-        move_left()
-        return False
-
 @task
 def task_5_2():
     while True:
--- lec02/lab/task_6.py
+++ lec02/lab/task_6.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_5_3():
-    pass
+    flag1 = False
+    flag2 = False
+    while True:
+        flag1 = wall_is_beneath()
+        if not wall_is_on_the_right():
+            move_right()
+            flag2 = wall_is_beneath()
+        if flag1 and not flag2: break
 
 
 if __name__ == '__main__':
