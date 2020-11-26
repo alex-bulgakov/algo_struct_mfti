48e72db7acb8e2c071de500ce52b68ab767d2a2d
alebul <master8423@gmail.com>
2020-11-26 15:01:36 +0300

    done task 07

--- lec02/lab/task_7.py
+++ lec02/lab/task_7.py
@@ -5,7 +5,20 @@ from pyrob.api import *
 
 @task
 def task_5_4():
-    pass
+    while True:
+        if not wall_is_beneath():
+            move_down()
+        else:
+            while True:
+                move_right()
+                if not wall_is_beneath():
+                    break
+            move_down()
+            while True:
+                move_left()
+                if wall_is_on_the_left():
+                    break
+            break
 
 
 if __name__ == '__main__':
