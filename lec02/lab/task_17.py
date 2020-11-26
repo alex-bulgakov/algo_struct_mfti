c5b471c2cd8af27b52bd874941b5e2358d7c1ea1
alebul <master8423@gmail.com>
2020-11-26 16:03:32 +0300

    done task 17

--- lec02/lab/task_17.py
+++ lec02/lab/task_17.py
@@ -5,7 +5,17 @@ from pyrob.api import *
 
 @task
 def task_8_27():
-    pass
+    while True:
+        if not wall_is_above():
+            if cell_is_filled():
+                move_left()
+                if cell_is_filled():
+                    break
+                move_right()
+                if cell_is_filled():
+                    move_right()
+                    break
+            move_up()
 
 
 if __name__ == '__main__':
