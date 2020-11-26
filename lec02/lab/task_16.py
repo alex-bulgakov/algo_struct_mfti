5cbc00337a8f727a9d0f483265f983779dd79a84
alebul <master8423@gmail.com>
2020-11-26 15:58:05 +0300

    done task 16

--- lec02/lab/task_16.py
+++ lec02/lab/task_16.py
@@ -2,10 +2,35 @@
 
 from pyrob.api import *
 
+def up():
+    while True:
+        if wall_is_above(): break
+        move_up()
+
+def down():
+    while True:
+        if wall_is_beneath(): break
+        move_down()
+
+def left():
+    while True:
+        if wall_is_on_the_left(): break
+        move_left()
+
+def right():
+    while True:
+        if wall_is_on_the_right(): break
+        move_right()
+
 
 @task
 def task_8_22():
-    pass
+    if not wall_is_above():
+        up()
+        if not wall_is_on_the_left():
+            left()
+        else:
+            right()
 
 
 if __name__ == '__main__':
