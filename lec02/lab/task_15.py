7e63ee31737de004db227e7505aa1b94ce504ca1
alebul <master8423@gmail.com>
2020-11-26 15:37:26 +0300

    done task 15

--- lec02/lab/task_15.py
+++ lec02/lab/task_15.py
@@ -5,7 +5,28 @@ from pyrob.api import *
 
 @task
 def task_8_21():
-    pass
+    n = 9
+    if wall_is_above() and wall_is_on_the_left():                       #top left
+        for i in range(n):
+            if wall_is_beneath() or wall_is_on_the_right(): break
+            move_down()
+            move_right()
+    elif wall_is_above() and wall_is_on_the_right():                    #top right
+        for i in range(n):
+            if wall_is_beneath() or wall_is_on_the_left(): break
+            move_down()
+            move_left()
+    elif wall_is_beneath() and wall_is_on_the_left():                     #bottom left
+        for i in range(n):
+            if wall_is_above() or wall_is_on_the_right(): break
+            move_up()
+            move_right()
+    elif wall_is_beneath and wall_is_on_the_right():                    #bottom right
+        for i in range(n):
+            if wall_is_above() or wall_is_on_the_left(): break
+            move_up()
+            move_left()
+
 
 
 if __name__ == '__main__':
